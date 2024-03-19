a8ac30fba638b33ade34e137e4f5a0a6c356afc3
John Morris <morri423@miamioh.edu>
2024-03-18 20:26:40 -0400

    Second commit

--- CSVNFLSearcher.java
+++ CSVNFLSearcher.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class CSVNFLSearcher {
     
-    private static final String file = "./Documents/nfl_teams_players - Players.csv";
+    private static final String file = "nfl_teams_players - Players.csv";
     
     public static void main(String[] args) throws FileNotFoundException {
-        searchPlayersByName("Tom Brady");
+        searchPlayersByName("Zay Flowers");
+        searchPlayersByName("Alex Highsmith");
+        searchPlayersByName("Patrick Mahomes");
     }
     
     public static void searchPlayersByName(String name) throws FileNotFoundException {
@@ -16,11 +18,15 @@ public class CSVNFLSearcher {
             read.useDelimiter(",");
             while(read.hasNext()) {
                 String inName = read.next();
-                if(inName.equals(name)) {
-                    String playerName = inName;
-                    String playerTeam = read.next();
+                if(inName.contains(name)) {
+                    String playerName = inName.substring(inName.indexOf(name.charAt(0)));
+                    String playerTeam = inName.substring(0, inName.indexOf(name.charAt(0)));
+                    System.out.println("Player: " + playerName + " Team: " + playerTeam);
+                    return;
                 }
             }
+            System.out.println("Player not found in file.");
+            
         } catch(FileNotFoundException e) {
             System.out.println("No connection to the CSV file.");
         }
